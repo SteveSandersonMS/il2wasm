be9a815d88da2cb574bd8cf06c604177a87d8a67
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2018-12-09 17:15:08 +0000

    Compile .NET dll to stubs that invoke AoT via JSInterop

--- il2wasm/Compiler.cs
+++ il2wasm/Compiler.cs
@@ -1,21 +1,28 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using Microsoft.JSInterop;
 using Mono.Cecil;
+using Mono.Cecil.Cil;
+using Mono.WebAssembly.Interop;
 
 namespace il2wasm
 {
     static class Compiler
     {
-        public static void Compile(string source, string dest)
+
+        public static void Compile(string source, string destDir)
         {
             var sourceModule = AssemblyDefinition.ReadAssembly(source).MainModule;
+            var destWithoutExtension = Path.Combine(destDir, Path.GetFileNameWithoutExtension(source));
 
             var wasmModule = CompileToWasm(sourceModule);
-            using (var fs = File.Create(dest))
+            using (var fs = File.Create($"{destWithoutExtension}.wasm"))
             {
                 wasmModule.WriteToBinary(fs);
             }
+
+            sourceModule.Write($"{destWithoutExtension}.dll");
         }
 
         private static WebAssembly.Module CompileToWasm(Mono.Cecil.ModuleDefinition sourceModule)
@@ -63,6 +70,63 @@ namespace il2wasm
 
             fn.Instructions.AddRange(
                 MethodBodyCompiler.Compile(sourceMethod, wasmBuilder, fn));
+
+            ReplaceWithJSInteropCall(sourceMethod);
+        }
+
+        private static void ReplaceWithJSInteropCall(MethodDefinition sourceMethod)
+        {
+            var body = sourceMethod.Body;
+            var instructions = body.Instructions;
+            instructions.Clear();
+            body.ExceptionHandlers.Clear();
+            
+            var jsRuntimeRef = sourceMethod.Module.ImportReference(typeof(JSRuntime));
+            var ijsRuntimeRef = sourceMethod.Module.ImportReference(typeof(IJSRuntime));
+            var monoWebAssemblyJSRuntimeRef = sourceMethod.Module.ImportReference(typeof(MonoWebAssemblyJSRuntime));
+
+            var ilProcessor = body.GetILProcessor();
+            ilProcessor.Append(
+                ilProcessor.Create(OpCodes.Call, new MethodReference("get_Current", ijsRuntimeRef, jsRuntimeRef)));
+            ilProcessor.Append(
+                ilProcessor.Create(OpCodes.Castclass, monoWebAssemblyJSRuntimeRef));
+
+            // Identifier
+            ilProcessor.Append(
+                ilProcessor.Create(OpCodes.Ldstr, sourceMethod.FullName));
+
+            // Other args
+            for (var i = 0; i < sourceMethod.Parameters.Count; i++)
+            {
+                ilProcessor.Append(
+                    ilProcessor.Create(OpCodes.Ldarg_S, sourceMethod.Parameters[i]));
+            }
+           
+            // Create closed-generic method reference
+            var methodRef = new MethodReference(
+                nameof(MonoWebAssemblyJSRuntime.InvokeUnmarshalled),
+                sourceMethod.ReturnType,
+                monoWebAssemblyJSRuntimeRef);
+            for (var i = 0; i < sourceMethod.Parameters.Count; i++)
+            {
+                methodRef.GenericParameters.Add(
+                    new GenericParameter($"T{i}", methodRef));
+            }
+            methodRef.GenericParameters.Add(
+                new GenericParameter("TRet", methodRef));
+            var genericMethodRef = new GenericInstanceMethod(
+                methodRef);
+            foreach (var p in sourceMethod.Parameters)
+            {
+                genericMethodRef.GenericArguments.Add(p.ParameterType);
+            }
+            genericMethodRef.GenericArguments.Add(sourceMethod.ReturnType);
+
+            // Invoke and return
+            ilProcessor.Append(
+                ilProcessor.Create(OpCodes.Callvirt, genericMethodRef));
+            ilProcessor.Append(
+                ilProcessor.Create(OpCodes.Ret));
         }
 
         private static WebAssembly.ValueType? ToWebAssemblyType(TypeReference dotNetType)
--- il2wasm/Program.cs
+++ il2wasm/Program.cs
@@ -9,15 +9,15 @@ namespace il2wasm
         {
             if (args.Length < 3)
             {
-                Console.Error.WriteLine($"Usage: dotnet exec {args[0]} -- <sourceassembly> <outputfile>");
+                Console.Error.WriteLine($"Usage: dotnet exec {args[0]} -- <sourceassembly> <outputdirectory>");
                 return 1;
             }
 
             var source = args[1];
-            var dest = args[2];
+            var destDir = args[2];
 
-            Directory.CreateDirectory(Path.GetDirectoryName(dest));
-            Compiler.Compile(source, dest);
+            Directory.CreateDirectory(destDir);
+            Compiler.Compile(source, destDir);
 
             return 0;
         }
