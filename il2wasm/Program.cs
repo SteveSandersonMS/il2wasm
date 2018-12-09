0fc9191fa86acd6a4093e0382bae72fb591330b5
Chris Bacon <chris@dunelm.org.uk>
2018-12-09 23:30:19 +0000

    Support VSCode on Linux

--- il2wasm/Program.cs
+++ il2wasm/Program.cs
@@ -7,9 +7,17 @@ namespace il2wasm
     {
         static void Main(string[] args)
         {
-            var root = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\..\samples"));
-            var source = Path.Combine(root, @"MyLibrary\bin\Debug\netstandard2.0\MyLibrary.dll");
-            var dest = Path.Combine(root, @"MyWebHost\wwwroot\MyLibrary.wasm");
+            // CWD is inconsistent between VS and `dotnet run` :(
+            // See https://github.com/dotnet/project-system/issues/3619
+            // Try VS way first: cwd is .exe file location
+            var root = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "..", "samples"));
+            if (!Directory.Exists(root))
+            {
+                // If dir doesn't exist, try `dotnet run` way: cwd is .csproj location
+                root = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "..", "samples"));
+            }
+            var source = Path.Combine(root, "MyLibrary", "bin", "Debug", "netstandard2.0", "MyLibrary.dll");
+            var dest = Path.Combine(root, "MyWebHost", "wwwroot", "MyLibrary.wasm");
 
             Compiler.Compile(source, dest);
         }
