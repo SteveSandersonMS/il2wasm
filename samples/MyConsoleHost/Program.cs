72e9aca09e316eb020676e1a70e28ec9cf972a81
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2018-12-10 10:59:26 +0000

    For perf comparisons, also add a console app that runs the same code

---
+++ samples/MyConsoleHost/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                var startTime = DateTime.Now;
+                var result = MyLibrary.Test.Run(2000);
+                var duration = DateTime.Now.Subtract(startTime).TotalMilliseconds;
+                Console.WriteLine($"Result: {result}; Duration: {duration}ms");
+            }
+        }
+    }
+}
