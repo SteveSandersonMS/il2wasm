7e6bc20b046887b8725656410533ab45dc50d3b1
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2018-12-11 16:59:27 +0000

    More cleanup

--- samples/MyConsoleHost/Program.cs
+++ samples/MyConsoleHost/Program.cs
@@ -6,12 +6,13 @@ namespace MyConsoleHost
     {
         static void Main(string[] args)
         {
+            const int n = 10000;
             for (var i = 0; i < 10; i++)
             {
                 var startTime = DateTime.Now;
-                var result = MyLibrary.Test.RunComputation(10000);
+                var result = MyLibrary.Test.RunComputation(n);
                 var duration = DateTime.Now.Subtract(startTime).TotalMilliseconds;
-                Console.WriteLine($"Result: {result}; Duration: {duration}ms");
+                Console.WriteLine($"The {n}th prime is: {result} (time: {duration}ms)");
             }
         }
     }
